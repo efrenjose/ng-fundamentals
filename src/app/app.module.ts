b19c5366784f8824ab93964e44a3c88a008fd50f
Efrén J. Rodríguez <efren.j.rodriguez@avanade.com>
2018-04-30 15:13:37 -0500

    create events-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { EventsComponent } from './events-app.component';
-
+import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    EventsComponent
+    EventsComponent,
+    EventsListComponent
   ],
   providers: [],
   bootstrap: [EventsComponent]
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'events-app',
   template: `<h2>Hello World</h2>
              <img src="/assets/images/basic-shield.png"/>
+             <events-list></events-list>
   `
 })
 export class EventsComponent {
